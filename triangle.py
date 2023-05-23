da1e2aaf39ab4360d14ad5fa0e13f57f4a3eb351
strangedin <36398275+strangedin@users.noreply.github.com>
2023-05-23 12:38:02 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,21 @@
+import time
+from flyt_python import api
+drone = api.navigation(timeout=120000)
+
+time.sleep(3)
+
+print 'taking off'
+drone.take_off(10.0)
+
+print 'starting mission'
+drone.position_set(10, 0, 0, relative=True)
+time.sleep(1)
+drone.position_set(-5, 8.6603, 0, relative=True)
+time.sleep(1)
+drone.position_set(-5, -8.6603, 0, relative=True)
+
+
+print 'Landing'
+drone.land(async=False)
+
+drone.disconnect()
